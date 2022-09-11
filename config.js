f05c30da9541070e5ebb5aa647d4813d5a63c419
ShiroNoMercy <108806813+Xavieroffcial@users.noreply.github.com>
2022-09-11 22:14:51 +0800

    Update config.js

--- config.js
+++ config.js
@@ -63,7 +63,7 @@ global.psaweria = 'https://saweria.com/DonateBot'
 global.nomorbot = '6283862006229'
 global.nomorown = '6283862006229'
 global.namebot = 'sʜɪʀᴏʜɪɢᴇ-ᴍᴅ'
-global.nameown = 'SkyxmoonOffc'
+global.nameown = 'ShiroNoMercy'
 
 
 /*============== STAFF ==============*/
@@ -124,8 +124,8 @@ global.author = '                「 sʜɪʀᴏʜɪɢᴇ-ᴍᴅ  あ⁩ 」'
 
 
 /*============== LOGO ==============*/
-global.thumb = 'https://telegra.ph/file/28a6c8c2d29bced9d11e5.jpg' //Main Thumbnail
-global.imagebot = 'https://telegra.ph/file/998ce20a7edd3b9a4e29d.jpg'
+global.thumb = 'https://telegra.ph/file/8710a022734e9001d4e90.jpg' //Main Thumbnail
+global.imagebot = 'https://telegra.ph/file/28a6c8c2d29bced9d11e5.jpg'
 global.giflogo = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4'
 global.thumbs = ['https://hardianto.xyz/api/anime/random?sfw=wallpaper&apikey=hardianto']
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
